e21aa78910391c20799de76ba4d6b7772433cb83
Kamil <KamilArkit@interia.pl>
2025-02-17 15:40:10 +0100

    Updated JsonData

--- src/main/java/com/example/demo/model/JsonData.java
+++ src/main/java/com/example/demo/model/JsonData.java
@@ -1,11 +1,27 @@
 package com.example.demo.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
 
+@Getter
+@Setter
 public class JsonData {
-    public boolean enableTry;
-    public String title;
-    public String intro;
-    public List<String> tags;
-    public String description;
+    @JsonProperty("enableTry")
+    private boolean enableTry;
+    @JsonProperty("title")
+    private String title;
+    @JsonProperty("intro")
+    private String intro;
+    @JsonProperty("description")
+    private String description;
+    @JsonProperty("url")
+    private String url;
+    @JsonProperty("tags")
+    private List<String> tags;
+
+    public JsonData() {
+    }
 }
